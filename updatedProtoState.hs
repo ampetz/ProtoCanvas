b2c0aefb724f793fe04705addaecf2ad596a2323
Adam Petz <apetz@g.emporia.edu>
2014-12-08 19:40:24 -0600

    initial commit for data staging functions

--- updatedProtoState.hs
+++ updatedProtoState.hs
@@ -1,7 +1,11 @@
 module ProtoState where
 
-import Data.Map hiding ((\\), null, map)
-import Data.List hiding (insert)
+import Prelude hiding (lookup)
+import Data.Map hiding ((\\), null, foldl)
+import qualified Data.Map as M
+import Data.List hiding (insert, lookup)
+import qualified Data.List as L
+
 import {-qualified-} Control.Monad.Trans.State {-as T-}
 import Control.Monad.Trans(liftIO)
 
@@ -15,7 +19,7 @@ type MessageId = Int
 type MessageText = String
 type To = Name --Destination
 data Contents = Send MessageText To
-              -- | Blank  --Action-send to self(vertical action bar with text beside?)--
+                | Action MessageText  --Action-send to self(vertical action bar with text beside?)--
                 deriving (Show, Eq)
 
 type Pos = Int --Represents a row(for messages) or column(for principals) position in the diagram
@@ -47,18 +51,154 @@ type Proto = StateT ProtoState IO
 data MaybeAfter = End
                   | After Pos
 
+stateMain = exec pTest
 
-addToPMap :: (Name, Principal) -> Proto ()
-addToPMap (n, p) = do
-  (ProtoState ps a b c d e f) <- get
-  let newPs = insert n p ps
-  put (ProtoState newPs a b c d e f)
+exec :: Proto () -> IO ProtoState
+exec p = execStateT p startState
+
+outputState :: Proto ()
+outputState = do
+  state <- get
+  liftIO $ putStrLn $ show state
+  liftIO $ putStrLn ""
+
+pTest :: Proto ()
+pTest = do
+  mds <- liftIO $ fromFile "testFile.txt"
+  --buildPrincipals mds 1
+  mc <- getMaxCol 
+  buildPrincipals mds (mc + 1)
+  outputState
+  removePrincipalCalled "1"
+  {-newNames <- getNewNames mds
+  liftIO $ mapM_ putStrLn newNames 
+
+  addPrincipalsAt 1 ["1","4"]
+  --outputState
+  addPrincipalsAt 2 ["2","3"]
+  --state <- get
+  outputState
+  --removePrincipalAt 3
+
+  addPrincipalsAt 1 ["1","5"]
+  outputState
+  --state' <- get
+  --liftIO $ putStrLn $ show $ state == state'
+  removePrincipalCalled "1"
+  removePrincipalCalled "2"
+  removePrincipalCalled "4"
+  removePrincipalCalled "5"
+  removePrincipalCalled "3" -}
+  return ()
+
+  
+pruneOutboxes :: Proto ()
+pruneOutboxes = do
+  ps <- getPrincipals
+  ms <- getMessages
+  let newPs = M.map (pruneOutbox ms) ps
+  setPMap newPs
   
 
-getPrincipals :: Proto (Map Name Principal)
-getPrincipals = do
-  state <- get
-  return (principals state)
+pruneOutbox :: Map MessageId Message -> Principal -> Principal
+pruneOutbox ms (Principal col obIn) =
+  --ms <- getMessages
+  let maybeIds = L.map (g ms) obIn
+      newOutbox = foldl f [] maybeIds in
+  (Principal col newOutbox)
+
+ where g :: Map MessageId Message -> MessageId -> (Maybe MessageId)
+       g ms mid = do
+         --ms <- getMessages
+         case member mid ms of
+           True -> Just mid
+           False -> Nothing
+      
+
+       f :: Outbox -> Maybe MessageId -> Outbox
+       f mids maybeId =
+         case maybeId of
+           Just mid -> mids ++ [mid]
+           Nothing -> mids
+
+removeMessage :: MessageId -> Proto ()
+removeMessage mid = do
+  ms <- getMessages
+  let newMMap = M.delete mid ms
+  setMMap newMMap
+  pushFreeMid mid
+
+
+  
+addMessagesAt :: Pos -> [MessageD] -> Proto ()
+addMessagesAt posIn [] = return ()
+addMessagesAt posIn (md:mds) = do
+
+
+  addMessageAt posIn md
+  addMessagesAt (posIn + 1) mds
+          
+          
+
+
+addMessageAt :: Pos -> MessageD -> Proto ()
+addMessageAt posIn (MessageD fr t m) = do
+  newMid <- getNewMid
+  upDownRows True posIn  
+  addToMMap (newMid, Message posIn (Send m t))    --Self action logic HERE!!!
+  incrMaxRow
+  ps <- getPrincipals
+  let maybeP = M.lookup fr ps
+  case maybeP of
+    Nothing -> return ()
+    Just (Principal _ outbox) -> do
+      let newP = Principal posIn (outbox ++ [newMid])
+          newPMap = M.insert fr newP ps
+      setPMap newPMap
+  
+
+  
+
+getNewMid :: Proto MessageId
+getNewMid = do
+  freeId <- popFreeMid
+  newMid <- case freeId of
+    Nothing -> do
+      maxMid <- getMaxMid
+      incrMaxMid
+      return (maxMid + 1)
+    Just fid -> return fid
+  return newMid
+
+
+
+buildPrincipals :: [MessageD] -> Pos -> Proto ()
+buildPrincipals mds pos = do
+  newNames <- getNewNames mds
+  addPrincipalsAt pos newNames
+  
+
+
+
+getNewNames :: [MessageD] -> Proto [Name]
+getNewNames mds = do
+  ps <- getPrincipals
+  let oldNames = keys ps
+      oldPlusNewNames = Prelude.foldl f oldNames mds
+      newNames = (\\) oldPlusNewNames oldNames
+  return newNames
+
+ where f :: [Name] -> MessageD -> [Name]
+       f ns md = let x = from md
+                     y = to md
+                     res = case x `elem` ns of
+                       True -> ns
+                       False -> ns ++ [x]
+                     res' = case y `elem` res of
+                       True -> res
+                       False -> res ++ [y]
+                 in res'
+
 
 doesPrincipalExist :: Name -> Proto Bool
 doesPrincipalExist name = do
@@ -66,31 +206,41 @@ doesPrincipalExist name = do
   return (member name ps)
   
 addPrincipalsAt :: Pos -> [Name] -> Proto ()
-addPrincipalsAt posIn ns@(name:rest)
-  | null ns = return ()
-  | otherwise = do
+addPrincipalsAt posIn [] = return ()
+addPrincipalsAt posIn (name:rest) = do
     existsPrior <- doesPrincipalExist name
     case existsPrior of
          True -> addPrincipalsAt posIn rest
          False -> do
            addPrincipalAt posIn name
            addPrincipalsAt (posIn + 1) rest
-               
+
 addPrincipalAt :: Pos -> Name -> Proto ()
 addPrincipalAt posIn name = do
   upDownCols True posIn
   addToPMap (name, Principal posIn [])
+  incrMaxCol
+
+removePrincipalCalled :: Name -> Proto ()
+removePrincipalCalled name = do
+  ps <- getPrincipals
+  case lookup name ps of
+    Nothing -> return ()
+    Just (Principal col _) -> removePrincipalAt col
+
 
 removePrincipalAt :: Pos -> Proto ()
 removePrincipalAt posIn = do
+  ps <- getPrincipals
+  let list = toList ps
+      newList = deleteBy remPred ("", Principal posIn []) list
+      newPmap = fromList newList
+  setPMap newPmap
   upDownCols False posIn
-
-
-
-updatePMap :: Map Name Principal -> Proto ()
-updatePMap newMap = do
-   (ProtoState _ a b c d e f) <- get
-   put (ProtoState newMap a b c d e f)
+  decrMaxCol
+ where
+   remPred :: (Name, Principal) -> (Name,Principal) -> Bool
+   remPred (_, Principal col _) (_, Principal col' _) = col == col'
 
 
 upDownCols :: Bool -> Pos -> Proto ()
@@ -99,30 +249,11 @@ upDownCols b posInserted = do
   let list :: [(Name, Principal)]
       list = toList ps
       updatedList = changeCols b posInserted list
-  updatePMap (fromList updatedList)
+  setPMap (fromList updatedList)
   return ()
-{-
-upCols :: Pos -> Proto ()
-upCols posInserted = do
-  ps <- getPrincipals
-  let list :: [(Name, Principal)]
-      list = toList ps
-      updatedList = incrCols posInserted list
-  updatePMap (fromList updatedList)
-  return ()
--}
 
 changeCols :: Bool -> Pos -> [(Name, Principal)] -> [(Name, Principal)] 
-changeCols b pos xs = map (change b pos) xs
-
-{-
-incrCols :: Pos -> [(Name, Principal)] -> [(Name, Principal)]
-incrCols pos xs = map (change True pos) xs
-
-decrCols :: Pos -> [(Name, Principal)] -> [(Name, Principal)]
-decrCols pos xs = map (change False pos) xs
--}
-
+changeCols b pos xs = L.map (change b pos) xs
 
 change :: Bool -> Pos -> (Name, Principal) -> (Name, Principal)
 change sign pos (name, pIn@(Principal col outbox)) =
@@ -134,6 +265,121 @@ change sign pos (name, pIn@(Principal col outbox)) =
         False -> pIn in
   (name, newP)
   
+upDownRows :: Bool -> Pos -> Proto ()
+upDownRows b posInserted = do
+  ms <- getMessages
+  let list :: [(MessageId, Message)]
+      list = toList ms
+      updatedList = changeRows b posInserted list
+  setMMap (fromList updatedList)
+  return ()
+
+--changeCols :: Bool -> Pos -> [(Name, Principal)] -> [(Name, Principal)] 
+changeRows b pos xs = L.map (change' b pos) xs
+
+change' sign pos (mid, mIn@(Message row contents)) =
+  let plusMinus = case sign of
+        True -> 1
+        False -> (-1)
+      newM = case row >= pos of
+        True -> Message (row + plusMinus) contents
+        False -> mIn in
+  (mid, newM)
+
+
+
+getMaxCol :: Proto Pos
+getMaxCol = do
+  state <- get
+  return $ maxCol state
+
+getMaxMid :: Proto MessageId
+getMaxMid = do
+  state <- get
+  return $ maxMessageId state
+
+
+setPMap :: Map Name Principal -> Proto ()
+setPMap newMap = do
+   (ProtoState _ a b c d e f) <- get
+   put (ProtoState newMap a b c d e f)
+
+setMMap :: Map MessageId Message -> Proto ()
+setMMap newMap = do
+   (ProtoState a _ b c d e f) <- get
+   put (ProtoState a newMap b c d e f)
+
+
+addToPMap :: (Name, Principal) -> Proto ()
+addToPMap (n, p) = do
+  (ProtoState ps a b c d e f) <- get
+  let newPs = insert n p ps
+  put (ProtoState newPs a b c d e f)
+
+--addToPMap :: (Name, Principal) -> Proto ()
+addToMMap (mid, m) = do
+  (ProtoState a ms c d e f g) <- get
+  let newMs = insert mid m ms
+  put (ProtoState a newMs c d e f g)
+  
+
+getPrincipals :: Proto (Map Name Principal)
+getPrincipals = do
+  state <- get
+  return (principals state)
+
+getMessages :: Proto (Map MessageId Message)
+getMessages = do
+  state <- get
+  return (messages state)
+
+getFreeMids :: Proto [MessageId]
+getFreeMids = do
+  state <- get
+  return (freeMids state)
+
+pushFreeMid :: MessageId -> Proto ()
+pushFreeMid mid = do
+  (ProtoState a b c d e fs g) <- get
+  put (ProtoState a b c d e (fs ++ [mid]) g)
+
+popFreeMid :: Proto (Maybe MessageId)
+popFreeMid = do
+  (ProtoState a b c d e fids@(i:rest) g) <- get
+  case null fids of
+    True -> return Nothing
+    False -> do
+      put (ProtoState a b c d e rest g)
+      return $ Just i
+
+removeFreeMid :: MessageId -> Proto ()
+removeFreeMid mid = do
+  (ProtoState a b c d e fs g) <- get
+  put (ProtoState a b c d e (L.delete mid fs) g)
+
+
+incrMaxCol :: Proto ()
+incrMaxCol = do
+  (ProtoState a b maxCol d e f g) <- get
+  put (ProtoState a b (maxCol+1) d e f g)
+
+incrMaxRow :: Proto ()
+incrMaxRow = do
+  (ProtoState a b c maxRow e f g) <- get
+  put (ProtoState a b c (maxRow+1) e f g)
+
+decrMaxCol :: Proto ()
+decrMaxCol = do
+  (ProtoState a b maxCol d e f g) <- get
+  put (ProtoState a b (maxCol-1) d e f g)
+
+incrMaxMid :: Proto ()
+incrMaxMid = do
+  (ProtoState a b c d maxMid f g) <- get
+  put (ProtoState a b c d (maxMid+1) f g)
+
+
+
 
 {-
 stateMain = exec pTest
